4bc380563fe8d582bdf208d61a5300e0d825820b
aayushjoglekar <aayushjoglekar@gmail.com>
2018-01-10 23:02:22 +0530

    Synced.

--- js/contestant.js
+++ js/contestant.js
@@ -1,13 +1,13 @@
- const app1=new Vue({
-          el : '#contestants',
-          data : {
-              contestants : []
-          },
-          created() {
-              fetch('https://opencode18.github.io/OpenCode-Collaborative/contestants.json')
-            .then(response => response.json() )
-            .then(json => {
-                this.contestants = json.contestants
-            })
-          }
+const app1=new Vue({
+    el : '#contestants',
+    data : {
+        contestants : []
+    },
+    created() {
+        fetch('https://opencode18.github.io/OpenCode-Collaborative/contestants.json')
+      .then(response => response.json() )
+      .then(json => {
+          this.contestants = json.contestants
       })
+    }
+})
