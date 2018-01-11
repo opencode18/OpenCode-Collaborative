35d698e404809611bb5c5cf4ebbe2f51bcd4d390
aayushjoglekar <aayushjoglekar@gmail.com>
2018-01-12 01:08:40 +0530

    Issue #102 : CSS problems Resolved. Back-card designed.

--- js/projects.js
+++ js/projects.js
@@ -4,7 +4,7 @@ const app2=new Vue({
               projects : []
           },
           created() {
-              fetch('https://opencode18.github.io/OpenCode-Collaborative/projects.json')
+              fetch('https://tlazypanda.github.io/OpenCode-Collaborative/projects.json')
             .then(response => response.json() )
             .then(json => {
                 this.projects = json.projects
