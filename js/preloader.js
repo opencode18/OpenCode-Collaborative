81fa0330c7f886b4ef7c33e190ec20ae3e8e2348
Yash Rathore <33805964+Re42@users.noreply.github.com>
2018-01-12 13:53:24 +0530

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -1,3 +1,8 @@
+$(document).ready(function($) {
+    var Body = $('body');
+    Body.addClass('preloader-site');
+});
 $(window).on('load', function () {
-      alert("Window Loaded");
- });
+    $('.preloader-wrapper').fadeOut();
+    $('body').removeClass('preloader-site');
+});
