4f2e9518805f9cc163453216ac762148a579b01d
Yash Rathore <33805964+Re42@users.noreply.github.com>
2018-01-15 21:32:53 +0530

    Update preloader.js

--- js/preloader.js
+++ js/preloader.js
@@ -1,8 +1,8 @@
 $(document).ready(function($) {
     var Body = $('body');
     Body.addClass('preloader-site');
-    var timeoutID = window.setTimeout(killPreload, 2000);
-});
+});    
+var timeoutID = window.setTimeout(killPreload, 2000);
 $(window).on('load',killPreload );
 function killPreload () {
     $('.preloader-wrapper').fadeOut();
