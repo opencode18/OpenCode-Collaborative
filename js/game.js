d68a2682b57e9020478ddbecd8941bfe235c2f80
Ananya Agrawal <33188930+ananya-agrawal@users.noreply.github.com>
2018-01-20 05:23:44 +0530

    added games to game.json (#364)
    * Update game.js
    
    * Add files via upload
    
    * Update game.json

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ const app3=new Vue({
         game : []
     },
     created() {
-        fetch('https://ananya-agrawal.github.io/OpenCode-Collaborative/json/game.json')
+        fetch('https://opencode18.github.io/OpenCode-Collaborative/json/game.json')
       .then(response => response.json() )
       .then(json => {
           this.game = json.game
