57b89a6927574d94b1a070570ee7d8fe41bead64
aadixiiita <34688457+aadixiiita@users.noreply.github.com>
2018-01-21 18:24:35 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -4,7 +4,7 @@ const app3=new Vue({
         game : []
     },
     created() {
-        fetch('http://aadishukla.me/OpenCode-Collaborative/json/game.json')
+        fetch('https://github.com/aadixiiita/OpenCode-Collaborative/blob/master/json/game.json')
       .then(response => response.json() )
       .then(json => {
           this.game = json.game
