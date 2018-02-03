7e9834e153e72b1a9a48af598a0be2a68e86ca44
Shivam Singhal <shivamsinghal0610@gmail.com>
2018-02-03 11:23:02 +0530

    Bettered code

--- js/reading-progressbar.js
+++ js/reading-progressbar.js
@@ -2,32 +2,27 @@ $(document).ready(function() {
     
   var getMax = function(){
     return $(document).height() - $(window).height();
-  }
+  };
     
   var getValue = function(){
     return $(window).scrollTop();
-  }
+  };
     
-  if ('max' in document.createElement('progress')) {
-    // Browser supports progress element
-    var progressBar = $('progress');
-        
-    // Set the Max attr for the first time
+  if ("max" in document.createElement('progress')) {
+    var progressBar = $("progress");
     progressBar.attr({ max: getMax() });
 
-    $(document).on('scroll', function(){
-      // On scroll only Value attr needs to be calculated
+    $(document).on("scroll", function(){
       progressBar.attr({ value: getValue() });
     });
-      
+    
     $(window).resize(function(){
-      // On resize, both Max/Value attr needs to be calculated
       progressBar.attr({ max: getMax(), value: getValue() });
     }); 
   
   } else {
 
-    var progressBar = $('.progress-bar'), 
+    var progressBar = $(".progress-bar"), 
         max = getMax(), 
         value, width;
         
@@ -35,7 +30,7 @@ $(document).ready(function() {
       // Calculate width in percentage
       value = getValue();            
       width = (value/max) * 100;
-      width = width + '%';
+      width = width + "%";
       return width;
     }
         
@@ -43,8 +38,8 @@ $(document).ready(function() {
       progressBar.css({ width: getWidth() });
     }
         
-    $(document).on('scroll', setWidth);
-    $(window).on('resize', function(){
+    $(document).on("scroll", setWidth);
+    $(window).on("resize", function(){
       // Need to reset the Max attr
       max = getMax();
       setWidth();
