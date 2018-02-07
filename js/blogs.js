6e6b1d9729843b3f4aec2d781c7e3bf588da72e6
Ace-Bansal <bansalekansh@gmail.com>
2018-02-08 01:00:37 +0530

    Add blogs section.

---
+++ js/blogs.js
@@ -0,0 +1,25 @@
+ const appblog=new Vue({
+          el : '#blogs',
+          data : {
+              blogs : []
+          },
+          created() {
+              fetch('https://opencode18.github.io/OpenCode-Collaborative/json/blogs.json')
+            .then(response => response.json() )
+            .then(json => {
+                this.blogs = json.blogs
+            })
+          },
+          methods: {
+            alphaSort: function (arr) {
+                return arr.sort(function (a, b) {
+                    var keyA = a.name,
+                        keyB = b.name;
+
+                    if (keyA < keyB) return -1;
+                    if (keyA > keyB) return 1;
+                    return 0;
+                });
+            }
+        }
+      });
