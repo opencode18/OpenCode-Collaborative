c3326813c744054b8da68ca1aa04f3fdab2607f6
Ekansh Bansal <34866483+Ace-Bansal@users.noreply.github.com>
2018-02-08 23:15:16 +0530

    Add Participants' Blogs (#463)
    * Add blogs section.
    
    * Rectify index.html
    
    * Rectify blog.js
    
    * Rectify blog.js
    
    * Rectify blogs.json
    
    * Rectify index.html
    
    * Rectify blogs.json
    
    * trial
    
    * Sync fork
    
    * Final commit

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,14 @@
- const app=new Vue({
-          el : '#mentors',
+ const appblog=new Vue({
+          el : '#blogs',
           data : {
-              mentors : []
+              blogs : []
           },
           created() {
-              fetch('https://opencode18.github.io/OpenCode-Collaborative/json/mentors.json')
+              fetch('https://opencode18.github.io/OpenCode-Collaborative/json/blogs.json')
             .then(response => response.json() )
             .then(json => {
-                this.mentors = json.mentors
-            })
+                this.blogs = json.blogs
+            });
           },
           methods: {
             alphaSort: function (arr) {
@@ -16,6 +16,7 @@
                     var keyA = a.name,
                         keyB = b.name;
 
+                    if (keyA == "OpenCode Collaborative") return 0;
                     if (keyA < keyB) return -1;
                     if (keyA > keyB) return 1;
                     return 0;
